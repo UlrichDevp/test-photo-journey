22f768b4d2015546a9bd97ecc486730037e26a0e
UlrichDevp <ulrichsoh72@gmail.com>
2026-06-20 00:22:25 +0100

    closingSection_mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,22 @@ import IntroCard from '../components/IntroCard';
 import BehindLens from "@/components/BehindLens";
 import FezSection from "@/components/FezSection"
 import Marrakech from "@/components/MarrakechSection";
-import InstaCard from "@/components/InstagramCards"
+import InstaCard from "@/components/InstagramCards";
+import ClosingSection from "@/components/ClosingSection";
 
-export default function Home(){
-  return(
- <div>
-    <Header/>
-    <IntroCard/>
-    <BehindLens/>
-    <FezSection/>
-    <Marrakech/>
-    <InstaCard/>
-    <Footer/>
-  </div>
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <IntroCard />
+      <BehindLens />
+      <FezSection />
+      <Marrakech />
+      <InstaCard />
+      <ClosingSection />
+      <Footer />
+
+    </div>
   )
- 
+
 }
